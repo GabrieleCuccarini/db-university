40fc0787bf9fe851fd77e639adfd5c6034f7310c
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2023-01-11 16:17:44 +0100

    2-4 done (half exercise completed)

--- query.sql
+++ query.sql
@@ -3,14 +3,17 @@ SELECT * FROM `students`
 WHERE YEAR(date_of_birth) = 1990;
 
 -- 2. Selezionare tutti i corsi che valgono più di 10 crediti(160)
-
+SELECT * FROM `courses`
+WHERE `cfu`>10;
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni(160)
 SELECT * FROM `students`
 WHERE `date_of_birth`<='1993/01/11';
 
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea(160)
-
+SELECT * FROM `courses`
+WHERE `period`='I semestre'
+	AND `year`='1';
 
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 
