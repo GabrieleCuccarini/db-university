862fee19817bda6adb1973412e48586f3de6817c
Gabriele Cuccarini <gabriele.cuccarini@gmail.com>
2023-01-12 22:09:56 +0100

    COMPLETED

--- join.sql
+++ join.sql
@@ -32,13 +32,49 @@ WHERE `teachers`.`id` = '44';
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il
 -- relativo dipartimento, in ordine alfabetico per cognome e nome
-
+SELECT  `students`.`surname` AS 'Cognome studente',
+		`students`.`name` AS 'Nome studente',
+        `degrees`.`name` AS 'Corso di Laurea',
+        `departments`.`name` AS 'Dipartimento'
+FROM `students`
+INNER JOIN `degrees`
+	ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments`
+	ON `degrees`.`department_id` = `departments`.`id`
+ORDER BY `students`.`surname` ASC, `students`.`name` ASC;
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
-
+SELECT `degrees`.`name` AS 'Corso di Laurea',
+		`courses`.`description` AS 'Corso',
+        `course_teacher`.`course_id` AS 'ID del Corso ',
+        `course_teacher`.`teacher_id` AS 'ID del professore',
+        `teachers`.`name` AS 'Nome professore',
+        `teachers`.`surname` AS 'Cognome professore'
+FROM `degrees`
+INNER JOIN `courses`
+	ON `degrees`.`id` = `courses`.`degree_id`
+INNER JOIN `course_teacher`
+	ON `courses`.`id` = `course_teacher`.`teacher_id`  
+INNER JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`; 
 
 -- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
-
+SELECT `departments`.`name` AS 'Nome Dipartimento',
+		`teachers`.`name` AS 'Nome professore',
+        `teachers`.`surname` AS 'Cognome professore',
+    	`course_teacher`.`teacher_id` AS 'ID del professore'
+FROM `departments`
+LEFT JOIN `degrees`
+	ON `departments`.`id` = `degrees`.`department_id`
+LEFT JOIN `courses`
+	ON `degrees`.`id` = `courses`.`degree_id`
+LEFT JOIN `course_teacher`
+	ON `courses`.`id` = `course_teacher`.`course_id`
+LEFT JOIN `teachers`
+	ON `teachers`.`id` = `course_teacher`.`teacher_id`
+WHERE `departments`.`name` = 'Dipartimento di Matematica'  
+GROUP BY `departments`.`name`,`teachers`.`name`,`teachers`.`surname`,`course_teacher`.`teacher_id`  
+-- (54 RISULTATI TROVATI)
 
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per
 -- superare ciascuno dei suoi esami
